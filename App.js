b27dc032e16e6f987573ed7713aca7302e3e4b7a
Augyu <yuhsuan@vt.edu>
2020-07-12 10:13:34 +0800

    Move code from app to list

--- App.js
+++ App.js
@@ -1,89 +1,26 @@
 import { StatusBar } from 'expo-status-bar'
-import React,{Component}from 'react'
-import { StyleSheet, Text, View, ListView,TextInput,Button,FlatList} from 'react-native'
-import { SwipeListView } from 'react-native-swipe-list-view'
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
 import List from './src/components/List'
 
-
 export default class MainActivity extends Component {
-  
-  constructor(props){
-
-      super(props)
-      this.state = {
-        inputtext  : '',
-        ListArray : [{title:"買咖啡"},{title:"買牛奶"}]
-      }
-  }
-
-  AddItemToArray =()=> {
-    const newItem = [...this.state.ListArray,this.state.inputtext]
-    this.setState({ListArray:newItem,inputtext:''})
-  }
-
-  handleRemoveItem=(index)=>{
-    const oldArray = this.state.ListArray
-    const newItem = oldArray.slice(index,1)
-    this.setState({ListArray:newItem})
-  }
-  
-  render(){
+  render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.TopTitle}>TO DO LIST</Text>
-        <SwipeListView
-          useFlatList
-          data={this.state.ListArray}
-          leftOpenValue={75}
-          rightOpenValue={-75}
-          renderHiddenRow={ data => (
-            <View style={styles.rowBack}>
-                <Text>刪除</Text>
-                <Text>刪除</Text>
-            </View>
-          )}
-          renderItem={({item}) => (
-            <Text style={styles.item}>
-              {item.title}
-            </Text>
-          )}
-          ></SwipeListView>
-        <TextInput 
-          style={styles.Textbox} 
-          placeholder = "請輸入文字"  
-          onChangeText={text => this.setState({ inputtext : {title:text}})} 
-          value = {this.state.inputtext}
-        />
-        <Button title="加入" onPress={this.AddItemToArray} />
-        {/*<List />*/}
-        {/* <Text style={styles.TopTitle}>TODO LIST</Text> */}
-        {/* <TextInput style={styles.Textbox} value={value}></TextInput>*/}
+        <List />
         <StatusBar style="auto" />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#ffffff',
-    paddingTop: 64,
-    paddingBottom : 400,
-    paddingLeft: 16,
+    padding: 16,
     alignItems: 'baseline',
     justifyContent: 'flex-start',
   },
-  TopTitle: {
-    fontSize: 32,
-  },
-  Textbox: {
-    fontSize : 16,
-    height: 32,
-    width: 100,
-  },
-  item: {
-    marginTop: 5,
-    fontSize : 24
-  }
 })
--- src/components/List.js
+++ src/components/List.js
@@ -1,46 +1,70 @@
-import React, { useState } from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  ListView,
-  TextInput,
-  FlatList,
-} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { SwipeListView } from 'react-native-swipe-list-view'
+import TodoItem from '../../constants/TodoItem'
 
 export default function List() {
-  const [data, setData] = useState([
-    { id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba', title: 'Buy coffee' },
-    { id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63', title: 'Clean the house' },
-    { id: '58694a0f-3da1-471f-bd96-145571e29d72', title: 'Walk the dog' },
+  const [inputText, setInputText] = useState('')
+  const [todoList, setTodoList] = useState([
+    { title: '買咖啡' },
+    { title: '買牛奶' },
   ])
+
+  const addItemToArray = () => {
+    if (inputText === '') return
+    const newItem = [...todoList, { title: inputText }]
+    setInputText('')
+    setTodoList(newItem)
+  }
+
+  const handleRemoveItem = (index) => {
+    const oldArray = todoList
+    const newItem = oldArray.slice(index, 1)
+    setTodoList(oldArray)
+  }
+
   return (
-    <View>
-      <Text style={styles.TopTitle}>TODO LIST</Text>
-      <FlatList
-        data={data}
+    <View style={styles.container}>
+      <Text style={styles.TopTitle}>TO DO LIST</Text>
+      <SwipeListView
+        useFlatList
+        data={todoList}
+        leftOpenValue={75}
+        rightOpenValue={-75}
+        renderHiddenRow={(data) => (
+          <View style={styles.rowBack}>
+            <Text>刪除</Text>
+            <Text>刪除</Text>
+          </View>
+        )}
         renderItem={({ item }) => (
-          <Text style={styles.item} keyExtractor={(item) => item.id}>
-            {item.title}{' '}
-          </Text>
-        )}></FlatList>
-      <TextInput style={styles.Textbox}></TextInput>
+          <Text style={styles.item}>{item.title}</Text>
+        )}></SwipeListView>
+      <TextInput
+        style={styles.Textbox}
+        placeholder="請輸入代辦事項"
+        onChangeText={(text) => setInputText(text)}
+        value={inputText}
+      />
+      <Button title="新增" onPress={() => addItemToArray()} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
   TopTitle: {
     fontSize: 32,
   },
   Textbox: {
+    fontSize: 16,
     height: 32,
-    width: 100,
-    borderColor: '#000000',
-    borderWidth: 1,
-    marginTop: 16,
   },
   item: {
-    marginTop: 5,
+    paddingTop: 8,
+    fontSize: 16,
   },
 })
